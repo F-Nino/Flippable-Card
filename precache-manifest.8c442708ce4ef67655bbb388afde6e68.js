cc17f4c22d906ffc21d4a428a3e3fb535586a2df
Felipe Nino <felipe@Felipes-MacBook-Pro.local>
2019-06-27 11:16:50 -0500

    Updates

---
+++ precache-manifest.8c442708ce4ef67655bbb388afde6e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c43e8197a9f0aad8709f012f08075e1",
+    "url": "/Flippable-Card/index.html"
+  },
+  {
+    "revision": "2a4932d56d7e474bf71d",
+    "url": "/Flippable-Card/static/css/main.1bda444e.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Flippable-Card/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "2a4932d56d7e474bf71d",
+    "url": "/Flippable-Card/static/js/main.26f19edb.chunk.js"
+  },
+  {
+    "revision": "d54b7b8fddcc6f42427b",
+    "url": "/Flippable-Card/static/js/runtime~main.94978b76.js"
+  },
+  {
+    "revision": "e84cedf8396beefe90606c2295b63059",
+    "url": "/Flippable-Card/static/media/AuLogo.e84cedf8.png"
+  }
+]);
